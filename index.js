0922cb580771c8da9b4a3a87ae466267523b8b1b
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-03-15 12:05:43 +0100

    I added everything

---
+++ index.js
@@ -0,0 +1,13 @@
+function show(para){
+    input.value += para
+}
+function equalTo(){
+    inp.value = eval(input.value)
+}
+function del(){
+    input.value = input.value.slice(0,-1)
+}
+function clean(){
+     input.value = input.value.slice(0,-0)
+     inp.value = inp.value.slice(0,-0)
+}   
